f1a902f8e3da4f06f2bd115cc7ec9922ffb90fe3
Santhosh1203122 <santhosh.m@madstreetden.com>
2020-06-01 14:23:18 +0530

    Removing the fit-content style property for fixing a bug

--- src/components/status-indicator/index.js
+++ src/components/status-indicator/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import {View, Text, StyleSheet } from 'react-native';
 import {Status} from '../../Constants';
 
+
+/* 
+    StatusIndicator is uesed to display the status of the task and the count of task completed
+        - First Indicator will have the status of the task ie TODO, In Progress etc
+            - Background colour changes according to the the status this data is fetched from the constants         
+        - Second Indicator will contain the no of task completed alon with the total no of tasks 
+    
+    Props:
+        1. statusId - status id of the particular task with will we can fetch the color and display name of the status
+        2. completed - total no of task completed
+        3. total -  total no of tasks
+    
+    Return: 
+        Will be returning 2 small cards which will be holding 50% width of the card
+*/
+
 function StatusIndicator(props) {
     const statusData = Status[props.statusId];
     return (
@@ -20,12 +36,11 @@ const styles = StyleSheet.create({
     holder: {
         flex: 1,
         flexDirection: 'row',
-        height: 'fit-content'
+        height: 25
     },
     indicator: {
         borderRadius: 2,
         padding: 5,
-        width: 'fit-content',
     },
     text: {
         fontSize: 10,
